103a4954345d2e824d69bbeb5f4126330ce2badd
Umberto Peserico <umberto.peserico@gmail.com>
2016-12-12 13:33:16 +0100

    Add order and dish models

---
+++ models/dish.js
@@ -0,0 +1,6 @@
+var Dish = function (name, photo, description, ingredients) {
+    this.name = name || "";
+    this.photo = photo || "";
+    this.description = description || "";
+    this.ingredients = ingredients || [];
+}
---
+++ models/order.js
@@ -0,0 +1,6 @@
+var Order = function (date, firstDish, secondDish, dessert) {
+    this.date = date || null;
+    this.firstDish = firstDish || null;
+    this.secondDish = secondDish || null;
+    this.dessert = dessert || null;
+}
